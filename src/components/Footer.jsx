9b7a841d65fcabfdd2fefc88940e9ef2fa36be92
aminulsohag92 <aminulsohag10292@gmail.com>
2026-04-16 10:52:12 +0600

    footer motion framer reset

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,14 +5,14 @@ import { fadeInUP, transition } from "../utils/animations";
 const Footer = () => {
   return (
     <>
-      <motion.footer
-        variants={fadeInUP}
-        initial="initial"
-        animate="animate"
-        transition={{ ...transition.default, delay: 0.4 }}
-        className=" py-8 px-4 md:px-6 bg-black border-t border-gray-800/80"
-      >
-        <div className="max-w-6xl mx-auto flex flex-col md:flex-row justify-between items-center text-sm text-gray-500">
+      <footer className=" py-8 px-4 md:px-6 bg-black border-t border-gray-800/80">
+        <motion.div
+          variants={fadeInUP}
+          initial="initial"
+          animate="animate"
+          transition={{ ...transition.default, delay: 0.4 }}
+          className="max-w-6xl mx-auto flex flex-col md:flex-row justify-between items-center text-sm text-gray-500"
+        >
           <p>&copy; 2026 All rights reserved.</p>
 
           <div className="flex gap-4 md:mt-0">
@@ -28,8 +28,8 @@ const Footer = () => {
               Instagram
             </a>
           </div>
-        </div>
-      </motion.footer>
+        </motion.div>
+      </footer>
     </>
   );
 };
